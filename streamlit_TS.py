b3b141325d2fce3261b9782a015e634a28b09b6b
Vyoma-Linguistic-Labs <108797006+Vyoma-Linguistic-Labs@users.noreply.github.com>
2024-10-14 10:29:25 +0530

    Update streamlit_TS.py
    Testing Goal Type and project columns with a print

--- streamlit_TS.py
+++ streamlit_TS.py
@@ -256,11 +256,11 @@ def get_selected_dates(start_date, end_date, key, open_google_sheet):
         # Extract the 'Task Name' column value for the current row
         task_name = row['Task Name']
         task_id = row['Task ID']
-
+        st.write([col for col in project_columns])
         if all(row[col] == 'nan' for col in project_columns):  # All specified columns are NaN
             rows_with_missing_data.append(task_name)
             row_id_with_missing_data.append(task_id)
-
+        st.write(row['Goal Type'])
         if row['Goal Type'] == 'nan':  # Goal Type is NaN
             rows_missing_goal_type.append(task_name)
             row_id_missing_goal_type.append(task_id)
